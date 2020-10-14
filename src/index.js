5eb9d6163c78578d2cbc1a9b74527dd8d63b967b
Saylance2503 <yurik_05@inbox.ru>
2020-10-14 19:12:00 +0400

    feat: added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function reverse (n) {
-  
+    let result = 0;
+    if (n > 0) {
+        while (n > 0) {
+            result = result * 10 + n % 10;
+            n = Math.floor(n / 10);
+        }
+        return result;
+    }
+    else {
+        while (Math.abs(n) > 0) {
+            result = result * 10 + Math.abs(n) % 10;
+            n = Math.floor(Math.abs(n) / 10);
+        }
+        return result;
+    }
+
 }
